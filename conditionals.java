a48b83e3785f77bc547ee1381fc337b01a67f829
Sarhak Virmani <virmanisarthak456@gmail.com>
2024-10-08 01:16:37 +0530

    Day 2 of learning java

---
+++ conditionals.java
@@ -0,0 +1,52 @@
+
+public class conditionals {
+    public static void main(String[] args) {
+        // conditionals if and else
+        int number = 2;
+        String result;
+
+        if (number % 2 == 0) { // check for even or odd number
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+
+        // we can do the sam by using ternary operator
+
+        result = (number % 2 == 0) ? "The number is even" : "The number is odd";
+
+        System.out.println("Result using ternary operator" + " " + result);
+
+        // Switch statement in JAVA
+
+        int numberofDay = 7;
+
+        switch (numberofDay) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+            System.out.println("A weak contains only 7 days");
+                break;
+        }
+
+    }
+}
---
+++ loops.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class loops {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        // Taking input from the user
+        System.out.println("How many numbers you want to print starting from 1");
+        int n = s.nextInt();
+        int i = 1;
+
+        // while loop in java
+        while (i<=n) {
+            System.out.println(i);
+            i++;
+        }
+
+        // for loop : Doing the same thing using a for loop
+        for (int j = 0; j < n; j++) {
+            System.out.println("Hi this segment is using for loop :" + j);
+        }
+
+    }
+
+}
